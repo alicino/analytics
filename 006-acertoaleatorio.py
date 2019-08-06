1516ee9a60f2a3d21f9942eeaf90c66755111da2
Alicino M <36045074+alicino@users.noreply.github.com>
2019-08-06 15:40:40 -0500

    Create 006-acertoaleatorio.py
    Original file

---
+++ 006-acertoaleatorio.py
@@ -0,0 +1,26 @@
+from random import randint
+
+lim_inf = 0
+lim_sup = 1000000
+contador = 0
+num_escolha = int(randint(lim_inf, lim_sup)) # Número a ser descoberto
+
+num_inicial = int(randint(lim_inf, lim_sup))
+# print(num_inicial)
+while (num_escolha != lim_inf) and (num_escolha != lim_sup):
+    contador += 1
+    if num_inicial in range(lim_inf, num_escolha):
+        lim_inf = num_inicial
+    else:
+        lim_sup = num_inicial
+
+    print(f'===== Next round: {contador} ====')
+    print(f'Novos limites: {lim_inf} - {lim_sup}')
+    print(f'Número escolha: ',num_escolha)
+    # print(f'Contador: ', contador)
+    num_inicial = int(randint(lim_inf, lim_sup))
+    
+fim = ' FIM '
+print(f'{fim:-^30}')
+print(f'Tentativas: ', contador)
+print(f'Número sorteado: ', num_escolha)
